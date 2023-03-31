1cc1f1001e783bf318012e9c3d28cb00b08223f1
Amy Lam <amyhlam2@gmail.com>
2023-03-31 15:07:13 -0700

    added new codewars challenge

---
+++ 8kyu/invert-values.js
@@ -0,0 +1,19 @@
+// Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+
+// invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
+// invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
+// invert([]) == []
+// You can assume that all values are integers. Do not mutate the input array/list.
+
+
+//Best solution 
+const invert = array => array.map(num => -num);
+
+//Another solution
+function invert(array) {
+    var newArray = [];
+    for (var i = 0; i < array.length; i++){
+        newArray.push(-array[i]);
+    }
+    return newArray;
+}
