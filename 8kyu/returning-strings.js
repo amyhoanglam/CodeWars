724027f8bb8c8b12a3674f3a5e7a30a03129dc0f
Amy Lam <amyhlam2@gmail.com>
2023-05-23 21:33:08 -0700

    return a string with user input

---
+++ 8kyu/returning-strings.js
@@ -0,0 +1,4 @@
+// Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+//Mine
+const greet = name => `Hello, ${name} how are you doing today?`
