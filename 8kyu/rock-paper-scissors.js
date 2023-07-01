a9b5d7a9d13fc9ddaa40d371b2ee22e9f20c2de2
Amy Lam <amyhlam2@gmail.com>
2023-07-01 11:58:58 -0700

    replace 0s if it is at the end

--- 8kyu/rock-paper-scissors.js
+++ 8kyu/rock-paper-scissors.js
@@ -25,29 +25,29 @@ const rps = (p1, p2) => {
     }
 };
 
-//Best 
-const rps = (p1, p2) => {
-    if (p1 === p2) return "Draw!";
-    var rules = {rock: "scissors", paper: "rock", scissors: "paper"};
-    if (p2 === rules[p1]) {
-      return "Player 1 won!";
-    }
-    else {
-      return "Player 2 won!";
-    }
-};
+// //Best 
+// const rps = (p1, p2) => {
+//     if (p1 === p2) return "Draw!";
+//     var rules = {rock: "scissors", paper: "rock", scissors: "paper"};
+//     if (p2 === rules[p1]) {
+//       return "Player 1 won!";
+//     }
+//     else {
+//       return "Player 2 won!";
+//     }
+// };
 
-//Another
-const rps = (p1, p2) => {
-    if (p1 == p2)
-      return 'Draw!';
+// //Another
+// const rps = (p1, p2) => {
+//     if (p1 == p2)
+//       return 'Draw!';
       
-     if (p1 == 'rock' && p2 == 'scissors') 
-       return 'Player 1 won!'
-     else if (p1 == 'scissors' && p2 == 'paper') 
-       return 'Player 1 won!'
-     else if (p1 == 'paper' && p2 == 'rock') 
-       return 'Player 1 won!'
-     else
-       return 'Player 2 won!';
-};
+//      if (p1 == 'rock' && p2 == 'scissors') 
+//        return 'Player 1 won!'
+//      else if (p1 == 'scissors' && p2 == 'paper') 
+//        return 'Player 1 won!'
+//      else if (p1 == 'paper' && p2 == 'rock') 
+//        return 'Player 1 won!'
+//      else
+//        return 'Player 2 won!';
+// };
